623ecf69bc192e7bd97d292e4c4acc06a9a72d10
viomjeet <viomjeet@email.com>
2024-12-27 20:18:36 +0530

    admin

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,430 @@
 "use client";
-import React from 'react'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { GrGroup } from "react-icons/gr";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { HiOutlineBell } from "react-icons/hi2";
 function page() {
+  let [activeUser, setActiveUser] = useState([]);
+  const router = useRouter();
+  useEffect(() => {
+    let activeUser = localStorage.getItem("user");
+    console.log(activeUser);
+    if (activeUser !== null) {
+      setActiveUser([]);
+      const getActiveUser = async () => {
+        try {
+          const res = await axios.get(`/api?username=${activeUser}`);
+          if (res.status === 200) {
+            const data: any = await res.data;
+            setActiveUser(data);
+          }
+        } catch (e: any) {
+          toast.error(e?.message);
+        }
+      };
+      getActiveUser();
+    } else {
+      router.push("/auth/login");
+    }
+  }, []);
+
+  const handleLogout = async () => {
+    let activeUser = localStorage.getItem("user");
+    if (activeUser !== null) {
+      let request: any = {
+        username: activeUser,
+      };
+      try {
+        const res = await axios.post(`/api`, request);
+        if (res.status === 200) {
+          toast.success(request.data);
+          localStorage.removeItem("user");
+          location.reload();
+        }
+      } catch (e: any) {
+        toast.error(e?.message);
+      }
+    }
+  };
+
   return (
-    <div>Dashboard</div>
-  )
+    // <div className="mx-auto w-full max-w-7xl lg:px-8">
+    //   <ToastContainer position="bottom-right" autoClose={1500} />
+    //   <div>
+    //     <code>{JSON.stringify(activeUser)}</code>
+    //   </div>
+    //   <button onClick={handleLogout}>Logout</button>
+    // </div>
+
+    <div className="flex h-screen bg-gray-200">
+      <div className="fixed inset-0 z-20 transition-opacity bg-black opacity-50 lg:hidden"></div>
+
+      <div className="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-900 lg:translate-x-0 lg:static lg:inset-0">
+        <div className="flex items-center justify-center mt-8">
+          <div className="flex items-center">
+            <svg
+              className="w-12 h-12"
+              viewBox="0 0 512 512"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M364.61 390.213C304.625 450.196 207.37 450.196 147.386 390.213C117.394 360.22 102.398 320.911 102.398 281.6C102.398 242.291 117.394 202.981 147.386 172.989C147.386 230.4 153.6 281.6 230.4 307.2C230.4 256 256 102.4 294.4 76.7999C320 128 334.618 142.997 364.608 172.989C394.601 202.981 409.597 242.291 409.597 281.6C409.597 320.911 394.601 360.22 364.61 390.213Z"
+                fill="#4C51BF"
+                stroke="#4C51BF"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>
+              <path
+                d="M201.694 387.105C231.686 417.098 280.312 417.098 310.305 387.105C325.301 372.109 332.8 352.456 332.8 332.8C332.8 313.144 325.301 293.491 310.305 278.495C295.309 263.498 288 256 275.2 230.4C256 243.2 243.201 320 243.201 345.6C201.694 345.6 179.2 332.8 179.2 332.8C179.2 352.456 186.698 372.109 201.694 387.105Z"
+                fill="white"
+              ></path>
+            </svg>
+
+            <span className="mx-2 text-2xl font-semibold text-white">
+              Dashboard
+            </span>
+          </div>
+        </div>
+
+        <nav className="mt-10">
+          <a
+            className="flex items-center px-6 py-2 mt-4 text-gray-100 bg-gray-700 bg-opacity-25"
+            href="#"
+          >
+            <svg
+              className="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"
+              ></path>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
+              ></path>
+            </svg>
+
+            <span className="mx-3">Dashboard</span>
+          </a>
+
+          <a
+            className="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+            href="#"
+          >
+            <svg
+              className="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"
+              ></path>
+            </svg>
+
+            <span className="mx-3">UI Elements</span>
+          </a>
+
+          <a
+            className="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+            href="#"
+          >
+            <svg
+              className="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+              ></path>
+            </svg>
+
+            <span className="mx-3">Tables</span>
+          </a>
+
+          <a
+            className="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+            href="#"
+          >
+            <svg
+              className="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+              ></path>
+            </svg>
+
+            <span className="mx-3">Forms</span>
+          </a>
+        </nav>
+      </div>
+      
+
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <header className="flex items-center justify-between px-6 py-4 bg-white border-b-4 border-indigo-600">
+          <div className="flex items-center">
+            <button className="text-gray-500 focus:outline-none lg:hidden">
+              dertyertyert
+            </button>
+
+            <div className="relative mx-4 lg:mx-0">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <svg
+                  className="w-5 h-5 text-gray-500"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                >
+                  <path
+                    d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                </svg>
+              </span>
+
+              <input
+                className="w-32 pl-10 pr-4 rounded-md form-input sm:w-64 focus:border-indigo-600"
+                type="text"
+                placeholder="Search"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <MenuButton className="flex mx-4 text-gray-600 focus:outline-none">
+                <HiOutlineBell className="text-2xl" />
+                </MenuButton>
+              </div>
+
+              <MenuItems
+                transition
+                className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+              >
+                <div className="py-1">
+                  <MenuItem>
+                  <a
+                  href="#"
+                  className="flex items-centerpx-4 py-4 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
+                >
+                  <img
+                    className="object-cover w-8 h-8 mx-1 rounded-full"
+                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80"
+                    alt="avatar"
+                  />
+                  <p className="mx-2 text-sm">
+                    <span className="font-bold">Sara Salah</span> replied on the{" "}
+                    <span className="font-bold text-indigo-400">
+                      Upload Image
+                    </span>{" "}
+                    artical . 2m
+                  </p>
+                </a>
+                  </MenuItem>
+                  
+                </div>
+              </MenuItems>
+            </Menu>
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <MenuButton className="relative block w-8 h-8 overflow-hidden rounded-full shadow focus:outline-none">
+                <img
+                  className="object-cover w-full h-full"
+                  src="https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=296&amp;q=80"
+                  alt="Your avatar"
+                />
+                  <ChevronDownIcon
+                    aria-hidden="true"
+                    className="-mr-1 size-5 text-gray-400"
+                  />
+                </MenuButton>
+              </div>
+
+              <MenuItems
+                transition
+                className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+              >
+                <div className="py-1">
+                  <MenuItem>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
+                    >
+                      Account settings
+                    </a>
+                  </MenuItem>
+                  <MenuItem>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
+                    >
+                      Support
+                    </a>
+                  </MenuItem>
+                  <MenuItem>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
+                    >
+                      License
+                    </a>
+                  </MenuItem>
+                  <form action="#" method="POST">
+                    <MenuItem>
+                      <button
+                        type="submit"
+                        className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
+                      >
+                        Sign out
+                      </button>
+                    </MenuItem>
+                  </form>
+                </div>
+              </MenuItems>
+            </Menu>
+          </div>
+        </header>
+
+        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+          <div className="container px-6 py-8 mx-auto">
+            <h3 className="text-3xl font-medium text-gray-700">Dashboard</h3>
+            <div className="mt-4">
+              <div className="flex flex-wrap -mx-6">
+                <div className="w-full px-6 sm:w-1/2 xl:w-1/3">
+                  <div className="flex items-center px-5 py-6 bg-white rounded-md shadow-sm">
+                    <div className="p-3 bg-indigo-600 bg-opacity-75 rounded-full">
+                      <GrGroup className="text-3xl text-white" />
+                    </div>
+
+                    <div className="mx-5">
+                      <h4 className="text-2xl font-semibold text-gray-700">
+                        8,282
+                      </h4>
+                      <div className="text-gray-500">New Users</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-8"></div>
+
+            <div className="flex flex-col mt-8">
+              <div className="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+                <div className="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
+                  <table className="min-w-full">
+                    <thead>
+                      <tr>
+                        <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                          Name
+                        </th>
+                        <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                          Title
+                        </th>
+                        <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                          Status
+                        </th>
+                        <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                          Role
+                        </th>
+                        <th className="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+                      </tr>
+                    </thead>
+
+                    <tbody className="bg-white">
+                      <tr>
+                        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                          <div className="flex items-center">
+                            <div className="flex-shrink-0 w-10 h-10">
+                              <img
+                                className="w-10 h-10 rounded-full"
+                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
+                                alt=""
+                              />
+                            </div>
+
+                            <div className="ml-4">
+                              <div className="text-sm font-medium leading-5 text-gray-900">
+                                John Doe
+                              </div>
+                              <div className="text-sm leading-5 text-gray-500">
+                                john@example.com
+                              </div>
+                            </div>
+                          </div>
+                        </td>
+
+                        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                          <div className="text-sm leading-5 text-gray-900">
+                            Software Engineer
+                          </div>
+                          <div className="text-sm leading-5 text-gray-500">
+                            Web dev
+                          </div>
+                        </td>
+
+                        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                          <span className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
+                            Active
+                          </span>
+                        </td>
+
+                        <td className="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
+                          Owner
+                        </td>
+
+                        <td className="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-no-wrap border-b border-gray-200">
+                          <a
+                            href="#"
+                            className="text-indigo-600 hover:text-indigo-900"
+                          >
+                            Edit
+                          </a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,61 @@
 "use client";
-import Link from "next/link";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { useRouter } from "next/navigation";
 function page() {
-  return <div className="mx-auto w-full max-w-7xl lg:px-8">
-    
-    <Link href="/dashboard/todos">Todos</Link>
+  let [activeUser, setActiveUser] = useState([]);
+  const router = useRouter();
+  useEffect(() => {
+    let activeUser = localStorage.getItem("user");
+    console.log(activeUser);
+    if (activeUser !== null) {
+      setActiveUser([]);
+      const getActiveUser = async () => {
+        try {
+          const res = await axios.get(`/api?username=${activeUser}`);
+          if (res.status === 200) {
+            const data: any = await res.data;
+            setActiveUser(data);
+          }
+        } catch (e: any) {
+          toast.error(e?.message);
+        }
+      };
+      getActiveUser();
+    } else {
+      router.push("/auth/login");
+    }
+  }, []);
 
-  </div>;
+  const handleLogout = async () => {
+    let activeUser = localStorage.getItem("user");
+    if (activeUser !== null) {
+      let request: any = {
+        username: activeUser,
+      };
+      try {
+        const res = await axios.post(`/api`, request);
+        if (res.status === 200) {
+          toast.success(request.data);
+          localStorage.removeItem("user");
+          location.reload();
+        }
+      } catch (e: any) {
+        toast.error(e?.message);
+      }
+    }
+  };
 
+  return (
+    <div className="mx-auto w-full max-w-7xl lg:px-8">
+      <ToastContainer position="bottom-right" autoClose={1500} />
+      <div>
+        <code>{JSON.stringify(activeUser)}</code>
+      </div>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
 }
 
 export default page;
